e290333430a1276f404d251be2ec7864f26447e9
APEX <73410320+FFFKLD@users.noreply.github.com>
2025-11-13 14:56:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,67 +2,12 @@ from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger, AstrBotConfig
 
-from buff163_unofficial_api import Buff163API
-from buff163_unofficial_api.models import Item, SpecificItem
-from buff163_unofficial_api.cs_enums import *
-from typing import List, Union
-from enum import Enum
+# 使用我们创建的新库
+from buff_api_cn import BuffApiCn
+from buff_api_cn.models import Item
 
 
-class CustomBuff163API(Buff163API):
-    """
-    继承官方 API 类，并重写部分方法以支持中文。
-    """
-    def get_featured_market(self, pageNum: int = 1) -> List[Item]:
-        """重写方法，在请求中加入 lang=zh-CN 参数"""
-        ep_params = {
-            "game": "csgo",
-            "lang": "zh-CN",
-            "page_num": str(pageNum)
-        }
-        result = self._rest_adapter.get(
-            endpoint="/market/goods", ep_params=ep_params
-        )
-        market = [Item(**item) for item in result.data["data"]["items"]]
-        return market
-
-    def get_item_market(
-        self,
-        category: Union[Knife, Gun, Glove, Agent, Sticker, OtherItem],
-        pageNum: int = 1,
-    ) -> List[Item]:
-        """重写方法，在请求中加入 lang=zh-CN 参数"""
-        if not isinstance(category, Enum):
-            raise TypeError("Category must be an instance of an Enum.")
-        
-        ep_params = {
-            "game": "csgo",
-            "lang": "zh-CN",
-            "page_num": str(pageNum),
-            "category": category.value
-        }
-        result = self._rest_adapter.get(
-            endpoint="/market/goods", ep_params=ep_params
-        )
-
-        market = [Item(**item) for item in result.data["data"]["items"]]
-        return market
-
-    def get_item(self, item_id: int) -> SpecificItem:
-        """重写方法，在请求中加入 lang=zh-CN 参数"""
-        ep_params = {
-            "game": "csgo",
-            "lang": "zh-CN",
-            "goods_id": str(item_id)
-        }
-        result = self._rest_adapter.get(
-            endpoint="/market/goods/info", ep_params=ep_params
-        )
-
-        return SpecificItem(**result.data["data"])
-
-
-@register("buff163", "Cline", "一个简单的 Buff163 查询插件", "1.0.1")
+@register("buff163", "Cline", "一个简单的 Buff163 查询插件", "1.0.2")
 class Buff163Plugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
@@ -70,8 +15,8 @@ class Buff163Plugin(Star):
         self.buff_api = None
         session_cookie = self.config.get("session_cookie")
         if session_cookie:
-            # 使用我们自定义的 API 类
-            self.buff_api = CustomBuff163API(session_cookie=session_cookie)
+            # 使用我们自定义的、支持中文的 API 类
+            self.buff_api = BuffApiCn(session_cookie=session_cookie)
         else:
             logger.warning("Buff163插件未配置 session_cookie，部分功能可能无法使用")
 
